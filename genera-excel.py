200d1c419470279c08851554349e57decd097bef
Jorge Navarrete <giomaussi@gmail.com>
2025-07-11 08:07:12 -0500

    min changes

--- genera-excel.py
+++ genera-excel.py
@@ -20,8 +20,8 @@ hoja_lista = wb_lista.active
 hoja_plantilla = wb_plantilla.active
 
 # Leer datos de la lista
-for fila in hoja_lista.iter_rows(min_row=2, values_only=True):  # asumiendo que la primera fila es encabezado
-    nombre, apellido, serie = fila[0], fila[1], fila[2]  # ajusta los índices según tu estructura
+for fila in hoja_lista.iter_rows(min_row=2, values_only=True):  # si la primera fila es encabezado ojo 
+    nombre, apellido, serie = fila[0], fila[1], fila[2]  
     
     # Crear una copia de la plantilla en memoria
     nuevo_wb = openpyxl.Workbook()
@@ -29,10 +29,9 @@ for fila in hoja_lista.iter_rows(min_row=2, values_only=True):  # asumiendo que 
     nueva_hoja = nuevo_wb.active
     
     # Aquí debes modificar la plantilla con los datos
-    # Ejemplo (ajusta las celdas según tu plantilla real):
-    nueva_hoja['A1'] = nombre      # Celda donde va el nombre
-    nueva_hoja['B1'] = apellido    # Celda donde va el apellido
-    nueva_hoja['C1'] = serie       # Celda donde va la serie
+    nueva_hoja['A1'] = nombre      
+    nueva_hoja['B1'] = apellido    
+    nueva_hoja['C1'] = serie       
     
     # Guardar el nuevo archivo
     nombre_archivo = f"{carpeta_salida}/{nombre}_{apellido}_{serie}.xlsx"
--- genera-excel2.py
+++ genera-excel2.py
@@ -19,18 +19,18 @@ wb_plantilla = load_workbook(archivo_plantilla)
 hoja_plantilla = wb_plantilla.active
 
 for index, row in df.iterrows():
-    nombre = row['NOMBRE']      # ajusta a tus nombres de columna
-    marca = row['MARCA']  # ajusta a tus nombres de columna
-    serie = row['SERIAL']        # ajusta a tus nombres de columna
+    nombre = row['NOMBRE'] 
+    marca = row['MARCA']  
+    serie = row['SERIAL']  
     
     # Crear copia de la plantilla
     nuevo_wb = load_workbook(archivo_plantilla)
     nueva_hoja = nuevo_wb.active
     
-    # Modificar plantilla (ajusta las celdas según necesites)
+    # Modificar plantilla 
     nueva_hoja['D5'] = nombre
     nueva_hoja['H5'] = str(marca) + ' / ' + str(serie)
-    #nueva_hoja['C1'] = serie
+    
     
     # Guardar archivo
     nombre_archivo = f"{carpeta_salida}/{nombre}_{marca}_{serie}.xlsx"
